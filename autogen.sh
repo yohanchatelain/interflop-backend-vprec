a1b19a9171a1440c76f8bed4cf5fd8ccb1ad3c42
Yohan Chatelain <yohan.chatelain@gmail.com>
2022-11-29 14:56:51 -0500

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -2,4 +2,11 @@
 autoreconf -is
 rm -rf interflop-stdlib
 git submodule update --init --recursive
-./install-stdlib.sh
+./install-stdlib.sh 2>&1 >install_stdlib.log
+if [ $? != 0 ]; then
+    echo "Error while installing interflop-stdlib"
+    cat install_stdlib.log
+    exit 1
+else
+    echo "Installing 'interflop-stdlib'"
+fi
